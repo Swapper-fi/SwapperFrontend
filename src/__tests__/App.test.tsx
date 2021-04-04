707bd05d4f23ab0fc5d2c18a83515b367ca3a2d9
syahrul12345 <muhdsyahrulnizam123@gmail.com>
2021-04-04 11:38:32 +0800

    Feat: Initialize tests for main page

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -3,5 +3,29 @@ import App from '../screens/App';
 
 test('Check if navigation bar exists', () => {
   const wrapper = shallow(<App />)
-  expect(wrapper.find(".navBar")).toBeTruthy()
+  const navbar = wrapper.find(".navBar")
+  expect(navbar).toHaveLength(1)
+})
+
+test('Navbar should have two children', () => {
+  const wrapper = shallow(<App />)
+  const navbar = wrapper.find(".navBar")
+  expect(navbar.children()).toHaveLength(2)
+})
+
+test('Navbar should have the div which contains the menu items', () => {
+  const wrapper = shallow(<App />)
+  const navbar = wrapper.find(".navBar")
+  const list = navbar.childAt(0)
+  expect(list).toHaveLength(1)
+  expect(list.children()).toHaveLength(5)
+
+})
+
+test('Navbar should have the version container', () => {
+  const wrapper = shallow(<App />)
+  const navbar = wrapper.find(".navBar")
+  const versionContainer = navbar.childAt(1)
+  expect(versionContainer).toHaveLength(1)
+  expect(versionContainer.children()).toHaveLength(2)
 })
