53bb124edf686230c69c4da98b9cf6034dade769
syahrul12345 <muhdsyahrulnizam123@gmail.com>
2021-04-14 16:19:32 +0800

    Feat: Added icon pack

---
+++ src/stories/Icons.stories.tsx
@@ -0,0 +1,50 @@
+import { Story, Meta } from '@storybook/react/types-6-0';
+import { Icon } from '../components/icon';
+import { IconProps } from '../components/icon/icon';
+export default {
+  title: 'Components/Icon',
+  component: Icon,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as Meta;
+
+const Template: Story<IconProps> = (args) => <Icon {...args} />;
+export const Home = Template.bind({})
+Home.args = {
+  type: "Home"
+}
+
+export const Activites = Template.bind({})
+Activites.args = {
+  type: "Activites"
+}
+
+export const Exchange = Template.bind({})
+Exchange.args = {
+  type: "Exchange"
+}
+
+export const Markets = Template.bind({})
+Markets.args = {
+  type: "Markets"
+}
+export const Notifications = Template.bind({})
+Notifications.args = {
+  type: "Notifications"
+}
+
+export const Promotions = Template.bind({})
+Promotions.args = {
+  type: "Promotions"
+}
+
+export const Settings = Template.bind({})
+Settings.args = {
+  type: "Settings"
+}
+
+export const Wallets = Template.bind({})
+Wallets.args = {
+  type: "Wallets"
+}
